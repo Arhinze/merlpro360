905372dbfd196195808cb74e74304088e805dc43
Arinze <francisokoye48@gmail.com>
2025-12-29 12:32:05 +0100

    adding social media pages

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -55,7 +55,7 @@ HTML;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <title>MERL PRO 360 | NGO Consultancy</title>
 
