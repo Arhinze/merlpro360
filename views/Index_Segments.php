85a54b2e4fecdc03f56f0112c0285e33f3d174f1
Arinze <francisokoye48@gmail.com>
2025-12-29 12:29:53 +0100

    adding social media pages

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -515,7 +515,7 @@ HTML;
                     <p style="margin-bottom: 10px;"><strong>Social Media:</strong><br></p>
                     <p><a href=""><i class="la la-instagram"></i> @merlpro360</a></p>
                     <p><a href=""><i class="la la-facebook"></i> @merlpro360</a></p>
-                    <p><a href=""><img src="/static/images/x-icon1.png"/> @merlpro360</a></p>
+                    <p><a href=""><i class="fa fa-twitter"></i> @merlpro360</a></p>
                     <p><a href=""><i class="fab fa-tiktok"></i> @merlpro360</a></p>
                 </div>
                 <div class="contact-form">
