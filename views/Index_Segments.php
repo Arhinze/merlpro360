22667bc46a23060a25a5440cb6e3fec333ade386
Arinze <francisokoye48@gmail.com>
2025-12-29 12:14:46 +0100

    adding social media pages

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -514,8 +514,8 @@ HTML;
                     <p style="margin-bottom: 10px;"><strong>Social Media:</strong><br></p>
                     <p><a href=""><i class="la la-instagram"></i> @merlpro360</a></p>
                     <p><a href=""><i class="la la-facebook"></i> @merlpro360</a></p>
-                    <p><a href=""><i class="la la-x"></i> @merlpro360</a></p>
-                    <p><a href=""><i class="la la-tiktok"></i> @merlpro360</a></p>
+                    <p><a href=""><img src="/static/images/x-icon.png"/> @merlpro360</a></p>
+                    <p><a href=""><img src="/static/images/tiktok-icon.png"/> @merlpro360</a></p>
                 </div>
                 <div class="contact-form">
                     <form method="post" action="/#contact">
@@ -572,8 +572,8 @@ HTML;
                     <ul>
                         <li><a href=""><i class="la la-instagram"></i> @merlpro360</a></li>
                         <li><a href=""><i class="la la-facebook"></i> @merlpro360</a></li>
-                        <li><a href=""><i class="la la-x"></i> @merlpro360</a></li>
-                        <li><a href=""><i class="la la-tiktok"></i> @merlpro360</a></li>
+                        <li><a href=""><img src="/static/images/x-icon.png"/> @merlpro360</a></li>
+                        <li><a href=""><img src="/static/images/tiktok-icon.png"/> @merlpro360</a></li>
                     </ul>
                 </div>
             </div>
